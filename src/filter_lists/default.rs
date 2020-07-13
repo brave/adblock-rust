d43dedb72460ed287d04a209c214f6b5bc2d89f0
fanboynz <mp3geek@gmail.com>
2020-07-13 03:54:12 +0200

    Remove Notification list from Brave

--- src/filter_lists/default.rs
+++ src/filter_lists/default.rs
@@ -94,17 +94,5 @@ pub fn default_lists() -> Vec<FilterList> {
             base64_public_key: String::from(""),
             desc: String::from(""),
         },
-        FilterList {
-            uuid: String::from("E55EAE71-8068-4903-A426-0F9EB3B332AC"),
-            url: String::from(
-                "https://easylist-downloads.adblockplus.org/fanboy-notifications.txt",
-            ),
-            title: String::from("Notification List"),
-            langs: Vec::new(),
-            support_url: String::from("https://easylist.to/"),
-            component_id: String::from(""),
-            base64_public_key: String::from(""),
-            desc: String::from(""),
-        },
     ].to_vec()
 }
