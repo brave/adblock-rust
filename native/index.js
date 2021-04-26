c32342ef78abb2beb6b62baee3d53fadd1e15024
Anton Lazarev <antonok35@gmail.com>
2021-04-26 13:55:06 -0700

    update filterset bindings

--- native/index.js
+++ native/index.js
@@ -1,7 +1,25 @@
 const blocker = require('./index.node');
 
+class FilterSet {
+    constructor(...args) {
+        this.boxed = blocker.FilterSet_constructor(...args);
+    }
+
+    addFilters(...args) {
+        return blocker.FilterSet_addFilters(this.boxed, ...args);
+    }
+
+    addFilter(...args) {
+        return blocker.FilterSet_addFilter(this.boxed, ...args);
+    }
+
+    intoContentBlocking(...args) {
+        return blocker.FilterSet_intoContentBlocking(this.boxed, ...args);
+    }
+}
+
 exports.FilterFormat = blocker.FilterFormat;
-exports.FilterSet = blocker.FilterSet;
+exports.FilterSet = FilterSet;
 exports.RuleTypes = blocker.RuleTypes;
 exports.Engine = blocker.Engine;
 exports.uBlockResources = blocker.uBlockResources;
--- native/src/lib.rs
+++ native/src/lib.rs
@@ -1,8 +1,9 @@
 use neon::prelude::*;
 use serde::{Deserialize, Serialize};
+use std::cell::RefCell;
 use std::path::Path;
 use adblock::engine::Engine;
-use adblock::lists::{RuleTypes, FilterFormat, FilterSet};
+use adblock::lists::{RuleTypes, FilterFormat, FilterSet as FilterSetInternal};
 use adblock::resources::Resource;
 use adblock::resources::resource_assembler::{assemble_web_accessible_resources, assemble_scriptlet_resources};
 
@@ -11,92 +12,113 @@ struct EngineOptions {
     pub optimize: Option<bool>,
 }
 
-declare_types! {
-    pub class JsFilterSet for FilterSet {
-        init(mut cx) {
-            match cx.argument_opt(0) {
-                Some(arg) => Ok(FilterSet::new(arg.downcast::<JsBoolean>().or_throw(&mut cx)?.value())),
-                None => Ok(FilterSet::default()),
-            }
-        }
-
-        method addFilters(mut cx) {
-            // Take the first argument, which must be an array
-            let rules_handle: Handle<JsArray> = cx.argument(0)?;
-            // Second argument is the optional format of the rules, defaulting to
-            // FilterFormat::Standard
-            let format = match cx.argument_opt(1) {
-                Some(format_arg) => neon_serde::from_value(&mut cx, format_arg)?,
-                None => FilterFormat::Standard,
-            };
-
-            // Convert a JsArray to a Rust Vec
-            let rules_wrapped: Vec<_> = rules_handle.to_vec(&mut cx)?;
-
-            let mut rules: Vec<String> = vec![];
-            for rule_wrapped in rules_wrapped {
-                let rule = rule_wrapped.downcast::<JsString>().or_throw(&mut cx)?
-                    .value();
-                rules.push(rule);
-            }
-
-            let mut this = cx.this();
-            let guard = cx.lock();
-            {
-                let mut filter_set = this.borrow_mut(&guard);
-                filter_set.add_filters(&rules, format);
-            }
-
-            Ok(JsNull::new().upcast())
-        }
-
-        method addFilter(mut cx) {
-            let filter: String = cx.argument::<JsString>(0)?.value();
-            let format = match cx.argument_opt(1) {
-                Some(format_arg) => neon_serde::from_value(&mut cx, format_arg)?,
-                None => FilterFormat::Standard,
-            };
-
-            let mut this = cx.this();
-            let guard = cx.lock();
-            let ok = {
-                let mut filter_set = this.borrow_mut(&guard);
-                filter_set.add_filter(&filter, format).is_ok()
-            };
-            // Return true/false depending on whether or not the filter could be added
-            Ok(JsBoolean::new(&mut cx, ok).upcast())
-        }
-
-        method intoContentBlocking(mut cx) {
-            let rule_types = match cx.argument_opt(0) {
-                Some(rule_types) => neon_serde::from_value(&mut cx, rule_types)?,
-                None => RuleTypes::default(),
-            };
-
-            let this = cx.this();
-            let guard = cx.lock();
-
-            let cloned = {
-                let filter_set = this.borrow(&guard);
-                filter_set.clone()
-            };
-
-            match cloned.into_content_blocking(rule_types) {
-                Ok((cb_rules, filters_used)) => {
-                    let cb_rules = neon_serde::to_value(&mut cx, &cb_rules)?;
-                    let filters_used = neon_serde::to_value(&mut cx, &filters_used)?;
-                    let js_result = JsObject::new(&mut cx);
-                    js_result.set(&mut cx, "contentBlockingRules", cb_rules)?;
-                    js_result.set(&mut cx, "filtersUsed", filters_used)?;
-                    Ok(js_result.upcast())
-                }
-                Err(_) => return Ok(JsUndefined::new().upcast()),
-            }
-        }
+#[derive(Default)]
+struct FilterSet(RefCell<FilterSetInternal>);
+impl FilterSet {
+    fn new(debug: bool) -> Self {
+        Self(RefCell::new(FilterSetInternal::new(debug)))
+    }
+    fn add_filters(&self, rules: &[String], format: FilterFormat) {
+        self.0.borrow_mut().add_filters(rules, format)
+    }
+    fn add_filter(&self, filter: &str, format: FilterFormat) -> Result<(), adblock::lists::FilterParseError> {
+        self.0.borrow_mut().add_filter(filter, format)
+    }
+    fn into_content_blocking(&self, rule_types: RuleTypes) -> Result<(Vec<adblock::content_blocking::CbRule>, Vec<String>), ()> {
+        self.0.borrow().clone().into_content_blocking(rule_types)
     }
 }
 
-declare_types! {
+impl Finalize for FilterSet {}
+
+fn create_filter_set(mut cx: FunctionContext) -> JsResult<JsBox<FilterSet>> {
+    match cx.argument_opt(0) {
+        Some(arg) => {
+            let debug: bool = arg.downcast::<JsBoolean, _>(&mut cx).or_throw(&mut cx)?.value(&mut cx);
+            Ok(cx.boxed(FilterSet::new(debug)))
+        }
+        None => Ok(cx.boxed(FilterSet::default())),
+    }
+}
+
+fn filter_set_add_filters(mut cx: FunctionContext) -> JsResult<JsNull> {
+    let this = cx.argument::<JsBox<FilterSet>>(0)?;
+
+    // Take the first argument, which must be an array
+    let rules_handle: Handle<JsArray> = cx.argument(1)?;
+    // Second argument is the optional format of the rules, defaulting to
+    // FilterFormat::Standard
+    let format = match cx.argument_opt(2) {
+        Some(format_arg) => match neon_serde::from_value(&mut cx, format_arg) {
+            Ok(v) => v,
+            Err(e) => cx.throw_error(e.to_string())?,
+        },
+        None => FilterFormat::Standard,
+    };
+
+    // Convert a JsArray to a Rust Vec
+    let rules_wrapped: Vec<_> = rules_handle.to_vec(&mut cx)?;
+
+    let mut rules: Vec<String> = vec![];
+    for rule_wrapped in rules_wrapped {
+        let rule = rule_wrapped.downcast::<JsString, _>(&mut cx).or_throw(&mut cx)?
+            .value(&mut cx);
+        rules.push(rule);
+    }
+
+    this.add_filters(&rules, format);
+
+    Ok(JsNull::new(&mut cx))
+}
+
+fn filter_set_add_filter(mut cx: FunctionContext) -> JsResult<JsBoolean> {
+    let this = cx.argument::<JsBox<FilterSet>>(0)?;
+
+    let filter: String = cx.argument::<JsString>(1)?.value(&mut cx);
+    let format = match cx.argument_opt(2) {
+        Some(format_arg) => match neon_serde::from_value(&mut cx, format_arg) {
+            Ok(v) => v,
+            Err(e) => cx.throw_error(e.to_string())?,
+        },
+        None => FilterFormat::Standard,
+    };
+
+    let ok = this.add_filter(&filter, format).is_ok();
+    // Return true/false depending on whether or not the filter could be added
+    Ok(JsBoolean::new(&mut cx, ok))
+}
+
+fn filter_set_into_content_blocking(mut cx: FunctionContext) -> JsResult<JsValue> {
+    let this = cx.argument::<JsBox<FilterSet>>(0)?;
+
+    let rule_types = match cx.argument_opt(1) {
+        Some(rule_types) => match neon_serde::from_value(&mut cx, rule_types) {
+            Ok(v) => v,
+            Err(e) => cx.throw_error(e.to_string())?,
+        },
+        None => RuleTypes::default(),
+    };
+
+    match this.into_content_blocking(rule_types) {
+        Ok((cb_rules, filters_used)) => {
+            let cb_rules = match neon_serde::to_value(&mut cx, &cb_rules) {
+                Ok(v) => v,
+                Err(e) => cx.throw_error(e.to_string())?,
+            };
+            let filters_used = match neon_serde::to_value(&mut cx, &filters_used) {
+                Ok(v) => v,
+                Err(e) => cx.throw_error(e.to_string())?,
+            };
+            let js_result = JsObject::new(&mut cx);
+            js_result.set(&mut cx, "contentBlockingRules", cb_rules)?;
+            js_result.set(&mut cx, "filtersUsed", filters_used)?;
+            Ok(js_result.upcast())
+        }
+        Err(_) => return Ok(JsUndefined::new(&mut cx).upcast()),
+    }
+}
+
+/*declare_types! {
     pub class JsEngine for Engine {
         init(mut cx) {
             // Take the first argument, which must be a JsFilterSet
@@ -259,9 +281,9 @@ declare_types! {
             Ok(js_value)
         }
     }
-}
+}*/
 
-fn validate_request(mut cx: FunctionContext) -> JsResult<JsBoolean> {
+/*fn validate_request(mut cx: FunctionContext) -> JsResult<JsBoolean> {
     let url: String = cx.argument::<JsString>(0)?.value();
     let source_url: String = cx.argument::<JsString>(1)?.value();
     let request_type: String = cx.argument::<JsString>(2)?.value();
@@ -308,20 +330,24 @@ fn build_rule_types_enum<'a, C: Context<'a>>(cx: &mut C) -> JsResult<'a, JsObjec
     rule_types_enum.set(cx, "COSMETIC_ONLY", cosmetic_only)?;
 
     Ok(rule_types_enum)
-}
+}*/
 
 register_module!(mut m, {
-    m.export_class::<JsFilterSet>("FilterSet")?;
-    m.export_class::<JsEngine>("Engine")?;
+    m.export_function("FilterSet_constructor", create_filter_set)?;
+    m.export_function("FilterSet_addFilters", filter_set_add_filters)?;
+    m.export_function("FilterSet_addFilter", filter_set_add_filter)?;
+    m.export_function("FilterSet_intoContentBlocking", filter_set_into_content_blocking)?;
+
+    /*m.export_class::<JsEngine>("Engine")?;
 
     m.export_function("validateRequest", validate_request)?;
-    m.export_function("uBlockResources", ublock_resources)?;
+    m.export_function("uBlockResources", ublock_resources)?;*/
 
-    let filter_format_enum = build_filter_format_enum(&mut m)?;
+    /*let filter_format_enum = build_filter_format_enum(&mut m)?;
     m.export_value("FilterFormat", filter_format_enum)?;
 
     let rule_types_enum = build_rule_types_enum(&mut m)?;
-    m.export_value("RuleTypes", rule_types_enum)?;
+    m.export_value("RuleTypes", rule_types_enum)?;*/
 
     Ok(())
 });
