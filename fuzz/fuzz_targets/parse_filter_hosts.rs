037cf3c059d606fb3effc6f7b81f1b11b392802b
Anton Lazarev <antonok35@gmail.com>
2021-09-16 18:37:01 -0700

    update fuzz targets to support ParseOptions

--- fuzz/fuzz_targets/parse_filter_hosts.rs
+++ fuzz/fuzz_targets/parse_filter_hosts.rs
@@ -1,9 +1,9 @@
 #![no_main]
 use libfuzzer_sys::fuzz_target;
-use adblock::lists::{parse_filter, FilterFormat};
+use adblock::lists::{parse_filter, FilterFormat, ParseOptions};
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(filter) = std::str::from_utf8(data) {
-        parse_filter(filter, true, FilterFormat::Hosts);
+        parse_filter(filter, true, ParseOptions { format: FilterFormat::Hosts, ..Default::default() });
     }
 });
