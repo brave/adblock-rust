58a4c51f11a906ef73ea1316923c723b6458e2d5
Mihai PLESA <mplesa@brave.com>
2022-06-02 14:09:22 -0500

    trying BRAVE_SERVICE_KEY

--- tests/live.rs
+++ tests/live.rs
@@ -106,7 +106,7 @@ fn get_blocker_engine_deserialized() -> Engine {
     use futures::FutureExt;
     let async_runtime = Runtime::new().expect("Could not start Tokio runtime");
 
-    let brave_service_key = std::env::var("brave_service_key")
+    let brave_service_key = std::env::var("BRAVE_SERVICE_KEY")
         .expect("Must set the $brave_service_key environment variable to execute live tests.");
 
     let dat_url = "https://adblock-data.s3.brave.com/4/rs-ABPFilterParserData.dat";
