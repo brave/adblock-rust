467fa5e43d96d92f6b508fe996c24044b40bd6c8
Anton Lazarev <antonok35@gmail.com>
2022-10-26 17:39:00 -0700

    reduce duplication in JS wrapper

--- native/index.js
+++ native/index.js
@@ -1,68 +1,28 @@
 const blocker = require('./index.node');
 
+// Exposes the specified native method names on `className`
+function forwardClassMethods(className, methods) {
+    for (const method of methods) {
+        className.prototype[method] = function(...args) {
+            const blocker_method = blocker[className.name + '_' + method];
+            return blocker_method(this.boxed, ...args);
+        }
+    }
+}
+
 class FilterSet {
     constructor(...args) {
         this.boxed = blocker.FilterSet_constructor(...args);
     }
-
-    addFilters(...args) {
-        return blocker.FilterSet_addFilters(this.boxed, ...args);
-    }
-
-    addFilter(...args) {
-        return blocker.FilterSet_addFilter(this.boxed, ...args);
-    }
-
-    intoContentBlocking(...args) {
-        return blocker.FilterSet_intoContentBlocking(this.boxed, ...args);
-    }
 }
+forwardClassMethods(FilterSet, ['addFilters', 'addFilter', 'intoContentBlocking']);
 
 class Engine {
     constructor(filter_set, ...args) {
         this.boxed = blocker.Engine_constructor(filter_set.boxed, ...args);
     }
-
-    check(...args) {
-        return blocker.Engine_check(this.boxed, ...args);
-    }
-
-    serializeRaw(...args) {
-        return blocker.Engine_serializeRaw(this.boxed, ...args);
-    }
-
-    serializeCompressed(...args) {
-        return blocker.Engine_serializeCompressed(this.boxed, ...args);
-    }
-
-    deserialize(...args) {
-        return blocker.Engine_deserialize(this.boxed, ...args);
-    }
-
-    enableTag(...args) {
-        return blocker.Engine_enableTag(this.boxed, ...args);
-    }
-
-    useResources(...args) {
-        return blocker.Engine_useResources(this.boxed, ...args);
-    }
-
-    tagExists(...args) {
-        return blocker.Engine_tagExists(this.boxed, ...args);
-    }
-
-    clearTags(...args) {
-        return blocker.Engine_clearTags(this.boxed, ...args);
-    }
-
-    addResource(...args) {
-        return blocker.Engine_addResource(this.boxed, ...args);
-    }
-
-    getResource(...args) {
-        return blocker.Engine_getResource(this.boxed, ...args);
-    }
 }
+forwardClassMethods(Engine, ['check', 'serializeRaw', 'serializeCompressed', 'deserialize', 'enableTag', 'useResources', 'tagExists', 'clearTags', 'addResource', 'getResource']);
 
 exports.FilterFormat = blocker.FilterFormat;
 exports.FilterSet = FilterSet;
