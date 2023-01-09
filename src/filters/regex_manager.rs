a9e8c2c0a2adf923587dff358a222e91c36fa698
Mikhail Atuchin <matuchin@brave.com>
2023-01-09 21:53:48 +0600

    Add regex_manager tests

--- src/filters/regex_manager.rs
+++ src/filters/regex_manager.rs
@@ -1,9 +1,17 @@
 //! A manager that creates/stores all regular expressions used by filters.
 //! Rarely used entries could be discarded to save memory.
 //! Non thread safe, the access must be synchronized externally.
-use std::{collections::HashMap, time::Instant};
-
+use std::collections::HashMap;
 use crate::filters::network::{compile_regex, CompiledRegex, NetworkFilter};
+use std::time::Duration;
+#[cfg(test)]
+use mock_instant::{Instant, MockClock};
+
+#[cfg(not(test))]
+use std::time::Instant;
+
+const REGEX_MANAGER_CLEAN_UP_INTERVAL: Duration = Duration::from_secs(30);
+const REGEX_MANAGER_DISCARD_TIME: Duration = Duration::from_secs(180);
 
 pub struct RegexDebugEntry {
     regex: String,
@@ -60,7 +68,8 @@ impl RegexManager {
                 v.last_used = self.now;
                 if matches!(v.regex, CompiledRegex::None) {
                     // A discarded entry, recreate it:
-                    v.regex = make_regexp(filter)
+                    v.regex = make_regexp(filter);
+                    self.compiled_regex_count += 1;
                 }
                 return v.regex.is_match(pattern);
             }
@@ -78,7 +87,7 @@ impl RegexManager {
 
     pub fn update_time(&mut self) {
         self.now = Instant::now();
-        if self.now - self.last_cleanup > std::time::Duration::from_secs(30) {
+        if self.now - self.last_cleanup >= REGEX_MANAGER_CLEAN_UP_INTERVAL {
             self.last_cleanup = self.now;
             self.cleanup();
         }
@@ -87,14 +96,14 @@ impl RegexManager {
     pub fn cleanup(&mut self) {
         let now = self.now;
         for (_, v) in &mut self.map {
-            if now - v.last_used < std::time::Duration::from_secs(180) {
+            if now - v.last_used >= REGEX_MANAGER_DISCARD_TIME {
                 // Discard the regex to save memory.
                 v.regex = CompiledRegex::None;
             }
         }
     }
 
-    #[cfg(feature = "debug-info")]
+    #[cfg(any(feature = "debug-info", test))]
     pub fn get_debug_regex_data(&self) -> Vec<RegexDebugEntry> {
         use itertools::Itertools;
         self.map.values().map(
@@ -104,8 +113,80 @@ impl RegexManager {
             .collect_vec()
     }
 
+    #[cfg(any(feature = "debug-info", test))]
     pub fn get_compiled_regex_count(&self) -> u64 {
         self.compiled_regex_count
+    }
+
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::{request, filters::network::NetworkMatchable};
+
+    fn make_filter(line: &str) -> NetworkFilter{
+        NetworkFilter::parse(line, true, Default::default()).unwrap()
+    }
+
+    fn make_request(url: &str) -> request::Request {
+        request::Request::from_url(url).unwrap()
+    }
+
+    fn get_active_regex_count(regex_manager: &RegexManager) -> i32 {
+        regex_manager.get_debug_regex_data().iter().fold(0,
+            |acc, x| if x.regex == CompiledRegex::None.to_string() {acc} else {acc + 1})
+    }
+
+    #[test]
+    fn simple_match() {
+        let mut regex_manager = RegexManager::default();
+        regex_manager.update_time();
+
+        let filter = make_filter("||geo*.hltv.org^");
+        assert!(filter.matches(
+            &make_request("https://geo2.hltv.org/"),
+            &mut regex_manager));
+        assert_eq!(get_active_regex_count(&regex_manager), 1);
+        assert_eq!(regex_manager.get_debug_regex_data().len(), 1);
+  }
+
+    #[test]
+    fn discard_and_recreate() {
+        let mut regex_manager = RegexManager::default();
+        regex_manager.update_time();
+
+        let filter = make_filter("||geo*.hltv.org^");
+        assert!(filter.matches(
+            &make_request("https://geo2.hltv.org/"),
+            &mut regex_manager));
+        assert_eq!(regex_manager.get_compiled_regex_count(), 1);
+        assert_eq!(get_active_regex_count(&regex_manager), 1);
+
+        MockClock::advance(REGEX_MANAGER_DISCARD_TIME - Duration::from_secs(1));
+        regex_manager.update_time();
+        // The entry shouldn't be discarded because was used during
+        // last REGEX_MANAGER_DISCARD_TIME.
+        assert_eq!(get_active_regex_count(&regex_manager), 1);
+
+        // The entry is entry is outdated, but should be discarded only
+        // in the next cleanup() call. The call was 2 sec ago and is throttled
+        // now.
+        MockClock::advance(REGEX_MANAGER_CLEAN_UP_INTERVAL - Duration::from_secs(1));
+        regex_manager.update_time();
+        assert_eq!(get_active_regex_count(&regex_manager), 1);
+
+        MockClock::advance(Duration::from_secs(2));
+        regex_manager.update_time();
+        // The entry is now outdated & cleanup() should be called => discard.
+        assert_eq!(get_active_regex_count(&regex_manager), 0);
+
+        // The entry is recreated, get_compiled_regex_count() increased +1.
+        assert!(filter.matches(
+            &make_request("https://geo2.hltv.org/"),
+            &mut regex_manager));
+        assert_eq!(regex_manager.get_compiled_regex_count(), 2);
+        assert_eq!(get_active_regex_count(&regex_manager), 1);
     }
 
 }
