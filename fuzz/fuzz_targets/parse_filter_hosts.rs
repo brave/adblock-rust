4752a3cc62bfc2b289af9206f87c33274b278ca0
Mikhail Atuchin <matuchin@brave.com>
2025-06-10 21:58:10 +0400

    cargo fmt

--- fuzz/fuzz_targets/parse_filter_hosts.rs
+++ fuzz/fuzz_targets/parse_filter_hosts.rs
@@ -1,11 +1,18 @@
 #![no_main]
 #![allow(unused_must_use)] // workaround for "error: unused `Result` that must be used"
 
-use libfuzzer_sys::fuzz_target;
 use adblock::lists::{parse_filter, FilterFormat, ParseOptions};
+use libfuzzer_sys::fuzz_target;
 
 fuzz_target!(|data: &[u8]| {
     if let Ok(filter) = std::str::from_utf8(data) {
-        parse_filter(filter, true, ParseOptions { format: FilterFormat::Hosts, ..Default::default() });
+        parse_filter(
+            filter,
+            true,
+            ParseOptions {
+                format: FilterFormat::Hosts,
+                ..Default::default()
+            },
+        );
     }
 });
