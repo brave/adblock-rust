43b21d28a932063042450d8384f8917c9bae47bd
Mikhail <matuchin@brave.com>
2026-05-13 01:51:08 +0400

    Set mask default to save memory (#639)
    The PR adjusts mask default value to consume less memory.
    Flatbuffer builder doesn't serialize the mask if it's equal to the default.
    The current default covers about 50% of the network rules and saves 200KB+ of memory.

--- tests/unit/engine.rs
+++ tests/unit/engine.rs
@@ -221,8 +221,8 @@ mod tests {
         #[cfg(feature = "debug-info")]
         {
             let debug_info = engine.get_debug_info();
-            let low_bound = 9_000_000;
-            let high_bound = 9_500_000;
+            let low_bound = 8_700_000;
+            let high_bound = 9_200_000;
             assert!(
                 debug_info.flatbuffer_size >= low_bound,
                 "Expected size >= {} bytes, got {}",
@@ -237,9 +237,9 @@ mod tests {
             );
         }
         let expected_hash: u64 = if cfg!(feature = "css-validation") {
-            255516435406374856
+            4233394045505187800
         } else {
-            11980507866898608383
+            1746450204048558433
         };
 
         assert_eq!(hash(&data), expected_hash, "{HASH_MISMATCH_MSG}");
