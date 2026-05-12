a55a96b2a856c0b0c404aa864445eec5f305edbe
Mikhail Atuchin <matuchin@brave.com>
2026-05-12 20:58:17 +0400

    Fix FilterPartIterator bug

--- src/filters/network.rs
+++ src/filters/network.rs
@@ -331,8 +331,8 @@ impl ExactSizeIterator for FilterPartIterator<'_> {
     fn len(&self) -> usize {
         match self.filter_part {
             FilterPart::Empty => 0,
-            FilterPart::Simple(_) => 1,
-            FilterPart::AnyOf(vec) => vec.len(),
+            FilterPart::Simple(_) => 1usize.saturating_sub(self.index),
+            FilterPart::AnyOf(vec) => vec.len().saturating_sub(self.index),
         }
     }
 }
